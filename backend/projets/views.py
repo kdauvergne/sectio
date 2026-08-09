9fb2120532742918925ea37adacdb480ec7506e0
kdauvergne <keysone.dauvergne@gmail.com>
2026-08-09 14:50:24 +0200

    (backend): mise à jour projets/views.py

--- backend/projets/views.py
+++ backend/projets/views.py
@@ -1,14 +1,21 @@
 from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.request import Request
 
-from .models import Projet
-from .serializers import ProjetSerializer
+from .models import Batiment, Niveau, Poteau, Projet
+from .serializers import (
+    BatimentDetailSerializer,
+    BatimentSerializer,
+    NiveauDetailSerializer,
+    NiveauSerializer,
+    PoteauSerializer,
+    ProjetDetailSerializer,
+    ProjetSerializer,
+)
 
 
 class ProjetViewSet(viewsets.ModelViewSet):
     """CRUD complet sur les projets."""
-
-    serializer_class = ProjetSerializer
 
     def get_queryset(self) -> QuerySet[Projet]:  # type: ignore[override]
         return Projet.objects.filter(membres=self.request.user)
@@ -16,3 +23,63 @@ class ProjetViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         projet = serializer.save()
         projet.membres.add(self.request.user)
+
+    def get_serializer_class(self):  # type: ignore[override]
+        if self.action == "retrieve":
+            return ProjetDetailSerializer
+        return ProjetSerializer
+
+
+class BatimentViewSet(viewsets.ModelViewSet):
+    """Bâtiments des projets dont l'utilisateur est membre."""
+
+    def get_serializer_class(self):  # type: ignore[override]
+        if self.action == "retrieve":
+            return BatimentDetailSerializer
+        return BatimentSerializer
+
+    def get_queryset(self) -> QuerySet[Batiment]:  # type: ignore[override]
+        queryset = Batiment.objects.filter(projet__membres=self.request.user)
+
+        projet = self.request.query_params.get("projet")  # type: ignore[override]
+        if projet:
+            queryset = queryset.filter(projet_id=projet)
+
+        return queryset.select_related("projet")
+
+
+class NiveauViewSet(viewsets.ModelViewSet):
+    """Niveaux des projets dont l'utilisateur est membre."""
+
+    request: Request
+
+    def get_serializer_class(self):  # type: ignore[override]
+        if self.action == "retrieve":
+            return NiveauDetailSerializer
+        return NiveauSerializer
+
+    def get_queryset(self) -> QuerySet[Niveau]:  # type: ignore[override]
+        queryset = Niveau.objects.filter(batiment__projet__membres=self.request.user)
+
+        batiment = self.request.query_params.get("batiment")
+        if batiment:
+            queryset = queryset.filter(batiment_id=batiment)
+
+        return queryset.select_related("batiment__projet")
+
+
+class PoteauViewSet(viewsets.ModelViewSet):
+    """Poteaux des projets dont l'utilisateur est membre."""
+
+    serializer_class = PoteauSerializer
+
+    def get_queryset(self) -> QuerySet[Poteau]:  # type: ignore[override]
+        queryset = Poteau.objects.filter(
+            niveau__batiment__projet__membres=self.request.user
+        )
+
+        niveau = self.request.query_params.get("niveau")  # type: ignore[override]
+        if niveau:
+            queryset = queryset.filter(niveau_id=niveau)
+
+        return queryset.select_related("niveau__batiment__projet")
